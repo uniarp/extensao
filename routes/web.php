bc90eb282bb5b5e550d2c3acb310febeb3eed512
Leticia Souza <leticiasouza2410@outlook.com>
2019-11-29 21:49:44 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,64 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+// $router->get('/', function() {
+//     return 'leticia' ;
+// });
+
+// $router->get('/validar/{chave}', function ($chave) {
+//     if ($chave === "mestre") {
+//         return '{status:true}' ;
+//     }
+//     return '{status:false}' ; 
+// });
+
+// $router->get('/imc/{altura}/{peso}', function ($altura,$peso) {
+//     $alturaQ = ($altura/100)*($altura/100);
+//     $imc = $peso/$alturaQ;
+//     $observacao = "ta bom";
+//     if ($imc <=20){
+//         $observacao = "visite sua vó";
+//     }
+//     if ($imc >= 30){
+//         $observacao = "não visite sua vó";
+//     }
+//     $resultado = (object)[
+//         'imc' => $imc,
+//         'observacao' => $observacao
+//     ];
+//     return json_encode ($resultado);
+// });
+
+$router->get('/voluntarios/cadastrar/{nome}/{email}/{cpf}/{telefone}/{ra}/{curso}', function ($nome, $email, $cpf, $telefone, $ra, $curso) {
+return true;
 });
+
+$router->get('/voluntarios/alterar/{nome}/{email}/{cpf}/{telefone}/{ra}/{curso}', function ($nome, $email, $cpf, $telefone, $ra, $curso) {
+    return true;
+    });
+
+$router->get('/voluntarios/listar', function () {
+return true;
+});
+
+$router->get('/voluntarios/excluir/{codVoluntario}', function ($codVoluntario) {
+return true;
+});
+
+$router->get('/validador/validarDocumento/{token}', function ($token) {
+return true;
+});
+
+$router->get('/documentos/listar/{filtros}', function ($filtros) {
+return true;
+});
+
+$router->get('/documentos/gerar/{codigoInscricao}', function ($codigoInscricao) {
+return true;
+});
+
+$router->get('/validador/validarDocumento/{token}', function ($token) {
+return true;
+});
+
+
