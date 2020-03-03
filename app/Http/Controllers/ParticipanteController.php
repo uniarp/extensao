ee9a60a720425bd5211df2a99ff96a60fcc29565
Mauricio da Silva <maurcioss83@gmail.com>
2020-03-03 20:00:47 -0300

    Teste Controller Participante

--- app/Http/Controllers/ParticipanteController.php
+++ app/Http/Controllers/ParticipanteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
 
-class Controller extends BaseController {
+class ParticipanteController extends BaseController {
     
     public function listarParticipante() {
         return app('db')->select("SELECT p.codparticipante cod, p.nome, p.cpf, p.ra, p.telefone, p.email FROM participante p;");
