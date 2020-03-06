abcc11ff818053290f8204600f85d658b6663fc3
MAURICIO ELYAN SOUZA DA SILVA <025379@uniarp.edu.br>
2020-03-06 20:33:42 -0300

    Testando ajuste no Controller de Participante

--- routes/web.php
+++ routes/web.php
@@ -193,13 +193,13 @@ $router->post('/participantes/cadastrar', function() {
     $participante = new ParticipanteController();
 
     try {
-        $participante->cadastrarParticipante($body['codParticipante'], $body['nome'], $body['cpf'], $body['ra'], $body['senha'],
+        $sql = $participante->cadastrarParticipante($body['codParticipante'], $body['nome'], $body['cpf'], $body['ra'], $body['senha'],
             $body['telefone'], $body['email']);
     } catch(Exception $e) {
         $response['erro'] = $e;
-        return json_encode($response);
+        return json_encode($sql);
     }
-    return $body;
+    return $sql;
 });
 
 $router->delete('/participantes/excluir/{codParticipante}', function($codParticipante) {
