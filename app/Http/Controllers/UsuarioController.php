fc2da77ab920819d10ce89920ba1a0932a94e0d0
Gabriel Soares <gabriel.mello2016@gmail.com>
2020-03-07 14:12:45 -0300

    Ajustado cadastro e pesquisa de usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -8,14 +8,14 @@ use Illuminate\Support\Facades\DB;
 class UsuarioController extends BaseController {
     
     public function listarUsuario() {
-        return app('db')->select("SELECT p.codparticipante cod, p.nome, p.cpf, p.ra, p.telefone, p.email FROM participante p;");
+        return app('db')->select("SELECT u.codUsuario, u.nome, u.email, u.cpf, u.senha FROM usuario u;");
     }
 
-    public function cadastrarUsuario($codUsuario, $nome, $email, $cpf, $cargo, $senha) {
+    public function cadastrarUsuario($codUsuario, $nome, $email, $cpf, $senha) {
         if ($codUsuario === null) {
-            // $query = 'INSERT INTO participante ("nome", "cpf", "ra", "senha", "telefone", "email") VALUES (';
-            // $query .= "'" . $nome . "', '" . $cpf . "', '" . $ra . "', '" . $senha . "', '" . $telefone . "', '" . $email . "');";
-            // return app('db')->select($query);
+            $query = 'INSERT INTO usuario ( "nome", "email", "cpf", "senha" ) VALUES (';
+            $query .= "'" . $nome . "', '" . $email . "', '" . $cpf . "', '" . $senha . "');";
+            return app('db')->select($query);
         } else {
             // $query = "UPDATE participante SET nome = '" . $nome . "', cpf = '" . $cpf . "', ra = '" . $ra . "', senha = '" . $senha . "', telefone = '" . $telefone . "', email = '" . $email . "' WHERE codparticipante = " . $codparticipante . ";";
             // return app('db')->select($query);
