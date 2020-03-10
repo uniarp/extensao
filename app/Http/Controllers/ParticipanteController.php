cae147ac963039cb255135a32b0155e5b017796b
MAURICIO ELYAN SOUZA DA SILVA <025379@uniarp.edu.br>
2020-03-09 21:13:18 -0300

    Excluir Participante

--- app/Http/Controllers/ParticipanteController.php
+++ app/Http/Controllers/ParticipanteController.php
@@ -11,14 +11,19 @@ class ParticipanteController extends BaseController {
         return app('db')->select("SELECT p.codparticipante cod, p.nome, p.cpf, p.ra, p.telefone, p.email FROM participante p;");
     }
 
-    public function cadastrarParticipante($codparticipante, $nome, $cpf, $ra, $senha, $telefone, $email) {
-        if ($codparticipante === null) {
+    public function cadastrarParticipante($codParticipante, $nome, $cpf, $ra, $senha, $telefone, $email) {
+        if ($codParticipante === null) {
             $query = 'INSERT INTO participante ("nome", "cpf", "ra", "senha", "telefone", "email") VALUES (';
             $query .= "'" . $nome . "', '" . $cpf . "', '" . $ra . "', '" . $senha . "', '" . $telefone . "', '" . $email . "');";
             return app('db')->select($query);
         } else {
-            $query = "UPDATE participante SET nome = '" . $nome . "', cpf = '" . $cpf . "', ra = '" . $ra . "', senha = '" . $senha . "', telefone = '" . $telefone . "', email = '" . $email . "' WHERE codparticipante = " . $codparticipante . ";";
+            $query = "UPDATE participante SET nome = '" . $nome . "', cpf = '" . $cpf . "', ra = '" . $ra . "', senha = '" . $senha . "', telefone = '" . $telefone . "', email = '" . $email . "' WHERE codparticipante = " . $codParticipante . ";";
             return app('db')->select($query);
         }
+    }
+
+    public function excluirParticipante($codParticipante) {
+        $query = "DELETE FROM participante WHERE codparticipante = '" . $codParticipante . "'";
+        return app('db')->select($query);
     }
 }
