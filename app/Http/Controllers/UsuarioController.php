00efa799595491062f4be8c1b7eccb61f714f293
Mauricio da Silva <maurcioss83@gmail.com>
2020-03-10 20:03:48 -0300

    Ajuste no Update de Usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -6,33 +6,29 @@ use Exception;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
 
-class UsuarioController extends BaseController
-{
+class UsuarioController extends BaseController {
 
-    public function listarUsuario()
-    {
+    public function listarUsuario() {
         return app('db')->select('SELECT u.codusuario "codUsuario", u.nome, u.email, u.cpf, u.senha FROM usuario u;');
     }
 
-    public function cadastrarUsuario($codUsuario, $nome, $email, $cpf, $senha)
-    {
-        try {
-            if ($codUsuario === null) {
-                $query = 'INSERT INTO usuario ( "nome", "email", "cpf", "senha" ) VALUES (';
-                $query .= "'" . $nome . "', '" . $email . "', '" . $cpf . "', '" . $senha . "');";
-                return app('db')->select($query);
-            } else {
-                // $query = "UPDATE participante SET nome = '" . $nome . "', cpf = '" . $cpf . "', ra = '" . $ra . "', senha = '" . $senha . "', telefone = '" . $telefone . "', email = '" . $email . "' WHERE codparticipante = " . $codparticipante . ";";
-                // return app('db')->select($query);
-            }
-        } catch (Exception $e) {
-            $responsea['erro'] = $e . 'Falha';
-            return $responsea;
+    public function cadastrarUsuario($codUsuario, $nome, $email, $cpf, $senha) {
+        if ($codUsuario === null) {
+            $query = 'INSERT INTO usuario ( "nome", "email", "cpf", "senha" ) VALUES (';
+            $query .= "'" . $nome . "', '" . $email . "', '" . $cpf . "', '" . $senha . "');";
+            return app('db')->select($query);
+        } else {
+            $query = "UPDATE usuario SET
+                    nome = '" . $nome . "',
+                    email = '" . $email . "',
+                    cpf = '" . $cpf . "',
+                    senha = '" . $senha . "'
+                    WHERE codusuario = " . $codUsuario . ";";
+            return app('db')->select($query);
         }
     }
 
-    public function excluirUsuario($codUsuario)
-    {
+    public function excluirUsuario($codUsuario) {
         $query = "DELETE FROM usuario WHERE codusuario = '" . $codUsuario . "'";
         return app('db')->select($query);
     }
