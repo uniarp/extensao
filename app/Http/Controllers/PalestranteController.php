27164ecc1928fde564f8ed169edddb299a616bd8
Gabriel Soares <gabriel.mello2016@gmail.com>
2020-03-11 20:25:13 -0300

    Ajuste no Gravar Palestrante, para incluir area

--- app/Http/Controllers/PalestranteController.php
+++ app/Http/Controllers/PalestranteController.php
@@ -29,6 +29,7 @@ class PalestranteController extends BaseController
             app('db')->select($query);
             $codPalestrante = app('db')->select('SELECT MAX(p.codpalestrante) as codpalestrante FROM palestrante p;');
             $codPalestrante = json_decode(json_encode($codPalestrante), true);
+            $area = json_decode($area, true);
             foreach ($area as $key) {
                 $codArea = $key['codArea'];
                 $codPalestrante = $codPalestrante['codpalestrante'];
