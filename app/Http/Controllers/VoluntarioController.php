8917f5648577f8a2deba6f0079f053408f496f68
Mauricio <maurcioss83@gmail.com>
2020-03-25 20:29:41 -0300

    Ajuste no update no voluntario

--- app/Http/Controllers/VoluntarioController.php
+++ app/Http/Controllers/VoluntarioController.php
@@ -19,9 +19,9 @@ class VoluntarioController extends BaseController {
     public function gravarVoluntario($codVoluntario, $nome, $email, $cpf, $telefone, $ra, $curso) {
         if ($codVoluntario === null) {
             $query = "INSERT INTO voluntario (nome, email, cpf, telefone, ra, curso) values(";
-            $query .= "'" . $nome . "', '" . $email . "', '" . $cpf . "', '" . $telefone . "', '" . $ra . "', '" . $curso . "');";
+            $query .= "'$nome', '$email', '$cpf', '$telefone', '$ra', '$curso');";
         } else {
-            $query = "UPDATE voluntario SET nome = '$nome', email = '$email', cpf = '$cpf', telefone = '$telefone', ra = '$ra', curso = '$curso' WHERE codvoluntario = $codVoluntario;";
+            $query = "UPDATE voluntario SET nome = '$nome', email = '$email', cpf = '$cpf', telefone = '$telefone', ra = '$ra', curso = $curso WHERE codvoluntario = $codVoluntario;";
         }
         return app('db')->select($query);
     }
