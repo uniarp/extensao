4ac5cbd84f4edfb74ed5153a7c334fa305e6c58f
Mauricio <maurcioss83@gmail.com>
2020-03-26 17:48:02 -0300

    Ajuste no listar participante por cod

--- app/Http/Controllers/ParticipanteController.php
+++ app/Http/Controllers/ParticipanteController.php
@@ -12,7 +12,7 @@ class ParticipanteController extends BaseController {
     }
 
     public function listarParticipante($codParticipante) {
-        return app('db')->select("SELECT p.codparticipante cod, p.nome, p.cpf, p.ra, p.telefone, p.email FROM participante p WHERE p.codparticipante = $codParticipante;");
+        return app('db')->select('SELECT p.codparticipante "codParticipante", p.nome, p.cpf, p.ra, p.telefone, p.email FROM participante p WHERE p.codparticipante = ' . $codParticipante . ';');
     }
 
     public function gravarParticipante($codParticipante, $nome, $cpf, $ra, $senha, $telefone, $email) {
