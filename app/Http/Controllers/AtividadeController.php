75c62d6f3da5a69343dee70500f896e937b72dd1
Gabriel Soares <gabriel.mello2016@gmail.com>
2020-03-28 12:04:13 -0300

    Criado metodos de lista, cadastro e alterar

--- app/Http/Controllers/AtividadeController.php
+++ app/Http/Controllers/AtividadeController.php
@@ -8,5 +8,54 @@ use Illuminate\Support\Facades\DB;
 
 class AtividadeController extends BaseController
 {
+    public function listarAtividade($codAtividade)
+    {
+        $atividade = app('db')->select('SELECT atv.codatividade "codAtividade", atv.titulo, atv.codtipo "codTipo", tpAtv.nome "nomeTipo",
+        atv.datainicio "dataInicio", atv.datafim "dataFim", atv.localizacao, atv.descricao
+        FROM atividade atv
+        JOIN tipoatividade tpAtv ON atv.codtipo = tpAtv.codtipoatividade WHERE atv.codatividade ='.$codAtividade.';');
+        $atividade = json_decode(json_encode($atividade), true);
+        for ($a = 0; $a < count($atividade); $a++) {
+            $palestranteAtividade = app('db')->select('SELECT atvPal.codatividadepalestrante "codAtividadePalestrante", atvPal.codpalestrante "codPalestrante", 
+            pales.nome FROM atividadepalestrante atvPal
+            JOIN palestrante pales ON atvPal.codpalestrante = pales.codpalestrante WHERE atvPal.codatividade =' . $codAtividade . ";");
+            $atividade[$a]['palestrante'] =  $palestranteAtividade;
+        }
+        return $atividade;
+    }
 
+    public function gravarPalestrante($codAtividade, $titulo, $codTipo, $dataInicio, $dataFim, $localizacao, $descricao, $palestrante)
+    {
+        if ($codAtividade === null) {
+            $query = 'INSERT INTO atividade ("titulo", "codtipo", "datainicio", "datafim", "localizacao", "descricao") VALUES (';
+            $query .= "'" . $titulo . "', ". $codTipo .", '" . $dataInicio . "', '" . $dataFim . "', '" . $localizacao . "', '" . $descricao . "');";
+            app('db')->select($query);
+            $codAtividade = app('db')->select('SELECT MAX(atv.codatividade) "codAtividade" FROM atividade atv;');
+            $codAtividade = json_decode(json_encode($codAtividade), true);
+            $codAtividade = $codAtividade[0]['codAtividade'];
+            for ($a = 0; $a < count($palestrante); $a++) {
+                $codPalestrante = $palestrante[$a]['CodPalestrante'];
+                $queryPalestrante = 'INSERT INTO atividadepalestrante ("codpalestrante", "codatividade") VALUES (';
+                $queryPalestrante .= "'" . $codPalestrante . "', '" . $codAtividade . "');";
+                app('db')->select($queryPalestrante);
+            }
+            $retorno = listarAtividade($codAtividade);
+            return $retorno;
+        } else {
+            $del = 'DELETE FROM atividadepalestrante  WHERE codatividade = ' . $codAtividade . ';';
+            app('db')->select($del);
+            for ($a = 0; $a < count($palestrante); $a++) {
+                $codPalestrante = $palestrante[$a]['codPalestrante'];
+
+                $queryPalestrante = 'INSERT INTO atividadepalestrante ("codpalestrante", "codatividade") VALUES (';
+                $queryPalestrante .= "'" . $codPalestrante . "', '" . $codAtividade . "');";
+                app('db')->select($queryPalestrante);
+            }
+            $query = "UPDATE atividade SET titulo = '" . $titulo . "', codtipo = '" . $codTipo . "', datainicio = '" . $dataInicio . "', datafim = '"
+            . $dataFim . "', localizacao = '" . $localizacao . "', descricao = '" . $descricao ."' WHERE codatividade =" . $codAtividade . ";";
+            app('db')->select($query);
+            $retorno = listarAtividade($codAtividade);
+            return $retorno;
+        }
+    }
 }
