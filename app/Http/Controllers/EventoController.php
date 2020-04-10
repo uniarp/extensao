36bd23b3d6e0412de436029ef52df3b9fa5d9089
Gabriel Soares <gabriel.mello2016@gmail.com>
2020-04-10 00:35:36 -0300

    Ajutado busca de atividade por codigo

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -21,13 +21,30 @@ class EventoController extends BaseController
 
     public function listarEvento($codEvento)
     {
-        return app('db')->select('SELECT
+        $evento = app('db')->select('SELECT
                                     e.codevento "codEvento", e.titulo,
                                     e.periodoinicial "periodoInicial", e.periodofinal "periodoFinal",
                                     e.inscricaoinicio "inscricaoInicio", e.inscricaofim "inscricaoFim",
                                     e.qtdmininscrito "qtdMinInscrito", e.qtdmaxinscrito "qtdMaxInscrito",
                                     e.modeldol "modeloDoc"
                                 FROM evento e WHERE e.codevento = ' . $codEvento . ';');
+        $evento['codArea'] = app('db')->select('SELECT ae.codarea "codArea", a.nome FROM areaevento ae JOIN area a ON ae.codarea = a.codarea
+            WHERE ae.codevento ='.$codArea.';');
+        $evento['voluntario'] = app('db')->select('SELECT ep.codvoluntario "codVoluntario", v.nome, v.email, v.cpf, v.telefone, v.ra, v.curso FROM equipeevento ep
+        JOIN voluntario v ON ep.codvoluntario = v.codvoluntario
+        WHERE ep.codevento =' .$codEvento. ';');
+        $atividade = app('db')->select('SELECT atv.codatividade "codAtividade", atv.titulo, atv.codtipo, atv.datainicio,
+            atv.datafim, atv.localizacao, atv.descricao FROM atividade atv WHERE atv.codevento ='.$codEvento.';');
+        $atividade = json_decode(json_encode($atividade), true);
+        for ($a = 0; $a < count($atividade); $a++) {
+            $codAtv = $atividade[$a]['codAtividade'];
+            $palestranteAtv = app('db')->select('SELECT ap.codatividadepalestrante "codAtividadePalestrante",ap.codpalestrante "codPalestrante", p.nome 
+            FROM atividadepalestrante ap JOIN palestrante p ON ap.codpalestrante = p.codpalestrante WHERE ap.codatividade ='.$codAtv.';');
+            $atividade[$a]['palestrante'] =  $palestranteAtv;
+        }
+        $evento['atividades'] = $atividade;
+        return $evento;
+
     }
 
     public function gravarEvento($codEvento, $titulo, $codArea, $periodoInicial, $periodoFinal, $inscricaoInicio, $inscricaoFim, $qtdMinInscrito, $qtdMaxInscrito, $modeloDoc, $voluntario, $atividades)
