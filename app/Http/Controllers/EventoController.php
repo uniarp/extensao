6ec81dfcc907209d2acbdf908a60a5422301302e
Maurício da Silva <maurcioss83@gmail.com>
2020-04-30 22:42:38 -0300

    Ajuste no metodo que lista inscritos

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -138,7 +138,8 @@ class EventoController extends BaseController
         $arrDados = json_decode(json_encode($arrDados), true);
         if (is_array($arrDados)) {
             foreach ($arrDados as $dado) {
-                $query = "UPDATE participanteevento SET presente = {$dado['presente']} WHERE codparticipante = {$dado['codParticipante']} AND codevento = {$dado['codEvento']};";
+                $presenca = $dado['presente'];
+                $query = "UPDATE participanteevento SET presente = $presenca WHERE codparticipante = {$dado['codParticipante']} AND codevento = {$dado['codEvento']};";
                 app('db')->select($query);
             }
         }
